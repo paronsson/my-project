534dde49a392a47d66ce6e67b155ca2cd5d041eb
pa <pa.aronsson@gmail.com>
2022-01-26 00:41:09 +0100

    qqeeqeqeeq

--- src/Page/Blog.elm
+++ src/Page/Blog.elm
@@ -113,7 +113,6 @@ viewPost data = [Html.h1 [style "text-align" "center"] [Html.b [] [text "BLOG"]]
                                        [Html.text (case (List.head (String.split "T" z.publish)) of
                                                                                        Just x -> x
                                                                                        Nothing -> ""),
-                                        Html.br [] [],
                                         Html.div [style "text-align" "center"] [text z.title],
                             viewMarkup z.body]]) data)
 
